61210a1f5614d7b5ab1fdf458c8427fd69a09a95
nattwat-wib <nattawat.wib@gmail.com>
2021-04-17 07:02:22 +0700

    add student page

---
+++ src/components/create-student.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CreateStudent extends Component {
+    render() {
+        return (
+            <div>
+                <p> React create student</p>
+            </div>
+        )
+    }
+}
---
+++ src/components/list.student.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class StudentList extends Component {
+    render() {
+        return (
+            <div>
+                <p> React StudentList student</p>
+            </div>
+        )
+    }
+}
