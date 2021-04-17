48f4d97df5e7a7c4d29e8eee8335a9a7f4f83fce
nattwat-wib <nattawat.wib@gmail.com>
2021-04-17 08:31:35 +0700

    clear

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import CreateStudent from './components/create-student';
+import EditStudent from './components/edit-student';
+import StudentList from './components/student-list';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <Navbar.Brand>
+                <Link to={'/create-student'} className="nav-link"> React MERN stack </Link>
+              </Navbar.Brand>
+
+              <Nav className="justify-content-end">
+                <Nav>
+                  <Link to={'/create-student'} className="nav-link">
+                    Create Student
+                  </Link>
+                </Nav>
+                <Nav>
+                  <Link to={'/student-list'} className="nav-link">
+                    Student List
+                  </Link>
+                </Nav>
+              </Nav>
+            </Container>
+          </Navbar>
+
+          <Container >
+            <Row>
+              <Col md="12">
+                <div className="wrapper">
+                  <Switch>
+                    <Route exact path="/" component={CreateStudent} />
+                    <Route path="/create-student" component={CreateStudent} />
+                    <Route path="/edit-student/:id" component={EditStudent} />
+                    <Route path="/student-list" component={StudentList} />
+                  </Switch>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+
+      </div>
+    </Router>
   );
 }
 
--- src/components/create-student.js
+++ src/components/create-student.js
@@ -1,10 +1,69 @@
-import React, { Component } from 'react'
-
+import React, { Component } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 export default class CreateStudent extends Component {
+    constructor(props) {
+        super(props) 
+
+        this.state = {
+            name: '',
+            email: '',
+            roll: ''
+        }
+    };
+
+    onChangeStudentName = (e) => {
+        this.setState({name: e.target.value});
+    };
+
+    onChangeStudentEmail = (e) => {
+        this.setState({email: e.target.value});
+    };
+
+    onChangeStudentRoll = (e) => {
+        this.setState({roll: e.target.value});
+    };
+
+    onSubmit = (e) => {
+        e.preventDefault();
+
+        console.log('student successfully created');
+        console.log(`student name : ${this.state.name} `);
+        console.log(`student email : ${this.state.email} `);
+        console.log(`roll no. : ${this.state.roll} `);
+
+        this.setState({
+            name: '',
+            email: '',
+            roll: ''
+        })
+    };
+
     render() {
         return (
-            <div>
-                <p> React create student</p>
+            <div className='form-wrapper mt-5'>
+                <h1> Create Student</h1>
+
+                <Form onSubmit={this.onSubmit}>
+                    <Form.Group controlId="name">
+                        <Form.Label> Name </Form.Label>
+                        <Form.Control type="text" value={this.state.name} onChange={this.onChangeStudentName} />
+                    </Form.Group>
+
+                    <Form.Group controlId="email">
+                        <Form.Label> email </Form.Label>
+                        <Form.Control type="text" value={this.state.email} onChange={this.onChangeStudentEmail} />
+                    </Form.Group>
+
+                    <Form.Group controlId="roll">
+                        <Form.Label> roll </Form.Label>
+                        <Form.Control type="text" value={this.state.roll} onChange={this.onChangeStudentRoll} />
+                    </Form.Group>
+
+                    <Button variant="success" size='lg' block="block" type="submit">
+                        Create student
+                    </Button>
+                </Form>
             </div>
         )
     }
